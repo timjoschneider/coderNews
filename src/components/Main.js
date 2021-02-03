8de925a401f7a41bd8cd905ef99ed4d0babc9009
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-02-03 10:03:00 +0100

    main.js and card.js cleaned up - iterating over data works and not hit s directly works now

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,45 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import MyCard from './MyCard';
-import dummy from '../data/dummy.json';
-//import SearchBar from './SearchBar';
+import {hits} from '../data/dummy.json';
 
 
 const Main = () => {
 
-    const [data, setData ] = useState({});
-    // console.log(data);
+    const [data, setData ] = useState([...hits]);
     // https://www.codecademy.com/courses/react-101/lessons/the-effect-hook/exercises/fetch-data
 
-    const fetchJSONData = () =>{
-        fetch('dummy.json', 
-        {
-            headers : { 
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
-        .then((response) => {
-            // console.log(response);
-            return response.json();
-          })
-          .then((myJson) => {
-            // console.log(myJson.hits);
-            setData(myJson);
-            console.log(`heree: ${data}`);
-          });
-      }
 
     useEffect(() => {
-        fetchJSONData()
-        // setData(dummy);
-        // console.log(`heree: ${data}`);
-    }, [])
+        let interval = setInterval(() => {
+            setData([...hits]);
+        }, 2000);
+        return () => clearInterval(interval);
+    }, []);
     
     const handleUserInput = (e) => {
-        // setUserInput({
-        //   ...userInput,
-        //   [e.target.name]: e.target.value
-        // });
         console.log(e.target.value);
       };
 
@@ -47,12 +24,10 @@ const Main = () => {
         <div className="container">
             
             <div className="row m-5">
-                {/* <SearchBar handleUserInput={handleUserInput}/> FOR EMELINE*/}
 
-                {dummy.hits.map((entry, i) => {
+                {data.map((entry, i) => {
                     return <MyCard key={i} data={entry}/>
                 })}
-                {/* <Card data={dummy.hits[0]}/> */}
             </div>
             
         </div>
