98892a6f2dc16a1bbf1fc45b09c81a7b1efcd2b2
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-02-08 17:22:31 +0100

    conditional rendering for single article

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,29 +3,29 @@ import { Col } from 'react-bootstrap';
 
 const Footer = () => {
   return (
-    <footer class="footer">     
-        <a class="footer-a" href="index.html" id="guidelines">
+    <footer className="footer">     
+        <a className="footer-a" href="index.html" id="guidelines">
           Guidelines
         </a>
-        <a class="footer-a" href="index.html">
+        <a className="footer-a" href="index.html">
           FAQ
         </a>
-        <a class="footer-a" href="index.html">
+        <a className="footer-a" href="index.html">
           Lists
         </a>
-        <a class="footer-a" href="index.html">
+        <a className="footer-a" href="index.html">
           API
         </a>
-        <a class="footer-a" href="index.html">
+        <a className="footer-a" href="index.html">
           Security
         </a>
-        <a class="footer-a" href="index.html">
+        <a className="footer-a" href="index.html">
           Legal
         </a>
-        <a class="footer-a" href="index.html">
+        <a className="footer-a" href="index.html">
           Apply to CN
         </a>
-        <a class="footer-a" href="https://github.com/EmelineLmr/coderNews#">
+        <a className="footer-a" href="https://github.com/EmelineLmr/coderNews#">
           Contact
         </a>
         <Col className="text-md-right" id="copyright">
--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
-const MyCard = ({ data }) => {
+const MyCard = ({ data, setArticleID }) => {
 
     return (
         <>
             <Card className="shadow mb-3">
                 <Card.Body>
                     <Card.Subtitle className="mb-2 text-muted">
-                        <a href={data.url} target="blank_" className="title pr-5">{data.title}</a> 
+                    <a href="#"  onClick={() => {setArticleID(data.objectID)}} target="blank_" className="title pr-5">
+                        {data.title}</a>
                     </Card.Subtitle>
                     <span className="card-span">{data.author}  </span> 
-                    <span className="card-span"> {data.created_at.substring(0, 10)}  </span>
-                    <span className="card-span"> {data.num_comments} Comments  </span>
+                    <span className="card-span m-5"> {data.created_at.substring(0, 10)}  </span>
+                    <span className="card-span m-3"> {data.num_comments} Comments  </span>
+                    <a href={data.url} target="blank_" className="pr-5  m-3">See original link</a> 
+                    
                 </Card.Body>
             </Card>
         </>
